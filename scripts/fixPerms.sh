f922bbd6be8faf1fa626b0c9e447bf208b035999
David Wayne Baxter <david@davidwaynebaxter.net>
2015-12-31 13:42:41 -0800

    Changes permissions on oc directory tro suppress errors during build.

--- scripts/fixPerms.sh
+++ scripts/fixPerms.sh
@@ -2,7 +2,7 @@ ocpath='/usr/share/webapps/owncloud'
 htuser='http'
 htgroup='http'
 find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
-find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
+find ${ocpath}/ -type d -print0 | xargs -0 chmod 0755
 chown -R root:${htuser} ${ocpath}/
 chown -R ${htuser}:${htgroup} ${ocpath}/apps/
 chown -R ${htuser}:${htgroup} ${ocpath}/config/
